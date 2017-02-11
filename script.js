7e621e2c443703a2209c90d26ee053319394767c
Adam Lezotte <adam@lezotte.me>
2017-02-11 14:47:57 -0500

    experiments

--- script.js
+++ script.js
@@ -1,18 +1,4 @@
-function shoppingList() {
-    var items = [
-        "Milk",
-        "Bread",
-        "Apples",
-        "Spinach"
-    ];
-    
-    var prices = [
-        2.49,
-        3.99,
-        4.49,
-        1.99
-    ];
-    
+function shoppingList(items, prices) {
     var total = 0;
     
     for ( i = 0; i < items.length; i++) {
@@ -23,4 +9,30 @@ function shoppingList() {
     console.log ( "Total: $" + total )
 }
 
-shoppingList();
+var items = [
+    "Milk",
+    "Bread",
+    "Apples",
+    "Spinach"
+];
+
+var prices = [
+    2.49,
+    3.99,
+    4.49,
+    1.99
+];
+
+shoppingList(items, prices);
+
+/*function addToList(form, items, prices) {
+    var item = form.itemBox.value;
+    var price = form.priceBox.value;
+    
+    // var items = [];
+    // var prices = [];
+    
+    items.push(item);
+    prices.push(price);
+    
+}*/
